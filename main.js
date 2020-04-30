db3d89742dcf4989dbcf03d85ef17e6bcfe59b73
tiapnn <tiapnn@yahoo.it>
2020-04-30 19:40:22 +0200

    added api call to get quotes

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 
 function timeFunction() {
-            setTimeout(function getQuote() {
+            setTimeout(function newGetQuote() {
 
-		let quotes = ["To be evenminded is the greatest virtue.", "Just keep in mind: the more we value things outside our control, the less control we have.", "The tranquility that comes when you stop caring what they say. Or think, or do. Only what you do.", "The whole future lies in uncertainty: live immediately.", "It does not matter what you bear, but how you bear it.", "A gem cannot be polished without friction, nor a man perfected without trials.", "To be calm is the highest achievement of the self."];
-		let authors = ["- Heraclitus ", "- Epictetus ", "- Marcus Aurelius", "- Seneca ", "- Seneca ", "- Seneca ", "- Zen proverb"];
+    let url = "https://randomstoicquotesapi.herokuapp.com/api/v1/quotes";
 
-		let randomN = Math.floor(Math.random() * quotes.length);
+	$.getJSON(url, function(data){
 
-		let randomQuote = quotes[randomN];
-		let randomAuth = authors[randomN];
+		let numeroACaso = Math.floor(Math.random() * data.data.length);
+		let currentQ = data.data[numeroACaso].attributes.text;
+		let quoteAuthId = data.data[numeroACaso].relationships.author.data.id;
+		let objAuth = data.included.filter( autor => autor.id == quoteAuthId);
+		let currentA = objAuth[0].attributes.name;
+		$('#text').html(currentQ);
+		$("#author").html(currentA);
 
-		$("#text").text('"' + randomQuote + '"');
-		$("#author").text(randomAuth);
-		
-	}, 600);
+	});
+}, 650);
         };
 
 
@@ -33,7 +35,10 @@ $(document).ready(function() {
 	
 	$(".btn").on("click", function() {
 		fadingOut();
-		fadingIn();
+		setTimeout(function() {
+			fadingIn();
+			}, 699);
+		
 	});
 	
 	fadingIn();
@@ -55,7 +60,42 @@ $(document).ready(function() {
 		let currentAuth = $("#author").text();
 		window.open('https://api.whatsapp.com/send?text='+currentQuote +' '+ currentAuth);
 	});
+
 });
 
 
+//old get quote func with no api request
 
+// function getQuote() {
+
+// 		let quotes = ["To be evenminded is the greatest virtue.", "Just keep in mind: the more we value things outside our control, the less control we have.", "The tranquility that comes when you stop caring what they say. Or think, or do. Only what you do.", "The whole future lies in uncertainty: live immediately.", "It does not matter what you bear, but how you bear it.", "A gem cannot be polished without friction, nor a man perfected without trials.", "To be calm is the highest achievement of the self."];
+// 		let authors = ["- Heraclitus ", "- Epictetus ", "- Marcus Aurelius", "- Seneca ", "- Seneca ", "- Seneca ", "- Zen proverb"];
+
+// 		let randomN = Math.floor(Math.random() * quotes.length);
+
+// 		let randomQuote = quotes[randomN];
+// 		let randomAuth = authors[randomN];
+
+// 		$("#text").text('"' + randomQuote + '"');
+// 		$("#author").text(randomAuth);
+		
+// 	}
+
+
+
+//new get quote func with api request 
+
+// function newGetQuote() {
+//	let url = "https://randomstoicquotesapi.herokuapp.com/api/v1/quotes";
+// 	$.getJSON(url, function(data){
+
+// 		let numeroACaso = Math.floor(Math.random() * data.data.length);
+// 		let currentQ = data.data[numeroACaso].attributes.text;
+// 		let quoteAuthId = data.data[numeroACaso].relationships.author.data.id;
+// 		let objAuth = data.included.filter( autor => autor.id == quoteAuthId);
+// 		let currentA = objAuth[0].attributes.name;
+// 		$('#text').html(currentQ);
+// 		$("#author").html(currentA);
+
+// 	});
+// };
